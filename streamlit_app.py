94a863976f23f1d0c65666efa1d7a166ecbe099a
Dalesrox <david.leonardo.espinosa@hotmail.com>
2025-02-12 20:53:33 -0500

    updated streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,7 +55,7 @@ def check_password():
     if "password_correct" not in st.session_state:
         # First run, show inputs for password.
         st.text_input(
-            "Password", type="password", on_change=password_entered, key="password"
+            "Password is:super_strong_password", type="password", on_change=password_entered, key="password"
         )
         return False
     elif not st.session_state["password_correct"]:
